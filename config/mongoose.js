acd4f7e91a7419f952d6dacd98c5019eb56badee
Professor <professor@uixlabs.in>
2023-07-25 01:08:06 +0530

    changes mongo db config url

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:admin@cluster0.dme40pl.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb://127.0.0.1/todo_list_db');
 const db = mongoose.connection;
 // error
 db.on('error',console.error.bind(console,'erroe connecting to db'));
